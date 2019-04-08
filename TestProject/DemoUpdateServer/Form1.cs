c113caecd000ca970bf66ad2ecaa8a0e29d0ccfe
dathlin <hsl200909@163.com>
2019-04-08 14:36:29 +0800

    feat(demeServer): add android client connection statistics

--- TestProject/DemoUpdateServer/Form1.cs
+++ TestProject/DemoUpdateServer/Form1.cs
@@ -111,6 +111,13 @@ namespace DemoUpdateServer
                 lognet.WriteInfo( $"{arg1.IpAddress.PadRight( 15 )} [{msg.PadRight( 8 )}] [{address}] Java" );
                 AddDict( address );
             }
+            else if (handle == 300)
+            {
+                simplifyServer.SendMessage( arg1, handle, version.ToString( ) );
+                string address = GetAddressByIp( arg1.IpAddress );
+                lognet.WriteInfo( $"{arg1.IpAddress.PadRight( 15 )} [{msg.PadRight( 8 )}] [{address}] Android" );
+                AddDict( address );
+            }
             else if (handle == 1000)
             {
                 // 返回统计信息
