6f98d35f3730567f82a673b4dcd9e74e7ef3f337
dathlin <hsl200909@163.com>
2019-06-17 20:37:42 +0800

    feat(CertificateDegree): add enum

---
+++ HslCommunication_Net45/Core/Types/CertificateDegree.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HslCommunication.Core.Types
+{
+    /// <summary>
+    /// 证书等级
+    /// </summary>
+    public enum CertificateDegree
+    {
+        /// <summary>
+        /// 只允许读取数据的等级
+        /// </summary>
+        Read = 1,
+
+        /// <summary>
+        /// 允许同时读写数据的等级
+        /// </summary>
+        ReadWrite = 2,
+    }
+}
