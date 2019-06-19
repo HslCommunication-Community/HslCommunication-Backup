fce22b75ff09e16985850eab75d1fcde761bf02d
dathlin <hsl200909@163.com>
2019-06-19 10:00:24 +0800

    feat(SerialBase): implement IDisposable interface

--- HslCommunication_Net45/Core/Net/NetworkBase/NetworkDoubleBase.cs
+++ HslCommunication_Net45/Core/Net/NetworkBase/NetworkDoubleBase.cs
@@ -619,7 +619,7 @@ namespace HslCommunication.Core.Net
                 if (disposing)
                 {
                     // TODO: 释放托管状态(托管对象)。
-                    InteractiveLock.Dispose( );
+                    InteractiveLock?.Dispose( );
                 }
 
                 // TODO: 释放未托管的资源(未托管的对象)并在以下内容中替代终结器。
--- HslCommunication_Net45/Serial/SerialBase.cs
+++ HslCommunication_Net45/Serial/SerialBase.cs
@@ -12,7 +12,7 @@ namespace HslCommunication.Serial
     /// <summary>
     /// 所有串行通信类的基类，提供了一些基础的服务
     /// </summary>
-    public class SerialBase
+    public class SerialBase : IDisposable
     {
         #region Constructor
 
@@ -333,8 +333,55 @@ namespace HslCommunication.Serial
 
         #endregion
 
+        #region IDisposable Support
+
+        private bool disposedValue = false; // 要检测冗余调用
+
+        /// <summary>
+        /// 释放当前的对象
+        /// </summary>
+        /// <param name="disposing">是否在</param>
+        protected virtual void Dispose( bool disposing )
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: 释放托管状态(托管对象)。
+                    hybirdLock?.Dispose( );
+                    SP_ReadData?.Dispose( );
+                }
+
+                // TODO: 释放未托管的资源(未托管的对象)并在以下内容中替代终结器。
+                // TODO: 将大型字段设置为 null。
+
+                disposedValue = true;
+            }
+        }
+
+        // TODO: 仅当以上 Dispose(bool disposing) 拥有用于释放未托管资源的代码时才替代终结器。
+        // ~SerialBase()
+        // {
+        //   // 请勿更改此代码。将清理代码放入以上 Dispose(bool disposing) 中。
+        //   Dispose(false);
+        // }
+
+        // 添加此代码以正确实现可处置模式。
+        /// <summary>
+        /// 释放当前的对象
+        /// </summary>
+        public void Dispose( )
+        {
+            // 请勿更改此代码。将清理代码放入以上 Dispose(bool disposing) 中。
+            Dispose( true );
+            // TODO: 如果在以上内容中替代了终结器，则取消注释以下行。
+            // GC.SuppressFinalize(this);
+        }
+        #endregion
+
+
         #region Private Member
-        
+
         private SerialPort SP_ReadData = null;                    // 串口交互的核心
         private SimpleHybirdLock hybirdLock;                      // 数据交互的锁
         private ILogNet logNet;                                   // 日志存储
