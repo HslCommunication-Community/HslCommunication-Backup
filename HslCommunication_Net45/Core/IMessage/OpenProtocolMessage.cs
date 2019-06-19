e24dbc042720b437681451fb9a044c0e6d4ac74b
dathlin <hsl200909@163.com>
2019-06-19 22:07:23 +0800

    feat(OpenProtocolNet): add some class, wait for finish

---
+++ HslCommunication_Net45/Core/IMessage/OpenProtocolMessage.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HslCommunication.Core.IMessage
+{
+    /// <summary>
+    /// OpenProtocol协议的消息
+    /// </summary>
+    public class OpenProtocolMessage : INetMessage
+    {
+        /// <summary>
+        /// 本协议的消息头长度
+        /// </summary>
+        public int ProtocolHeadBytesLength
+        {
+            get { return 4; }
+        }
+
+        /// <summary>
+        /// 头子节信息
+        /// </summary>
+        public byte[] HeadBytes { get; set; }
+
+        /// <summary>
+        /// 内容字节信息
+        /// </summary>
+        public byte[] ContentBytes { get; set; }
+
+        /// <summary>
+        /// 检查接收的数据是否合法
+        /// </summary>
+        /// <param name="token">令牌</param>
+        /// <returns>是否合法</returns>
+        public bool CheckHeadBytesLegal( byte[] token )
+        {
+            if (HeadBytes == null) return false;
+
+            return true;
+        }
+
+        /// <summary>
+        /// 从头子节信息中解析出接下来需要接收的数据长度
+        /// </summary>
+        /// <returns>接下来的数据长度</returns>
+        public int GetContentLengthByHeadBytes( )
+        {
+            if (HeadBytes?.Length >= 4)
+            {
+                return Convert.ToInt32( Encoding.ASCII.GetString( HeadBytes, 0, 4 ) ) - 4;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        /// <summary>
+        /// 获取头子节里的特殊标识
+        /// </summary>
+        /// <returns>标识信息</returns>
+        public int GetHeadBytesIdentity( )
+        {
+            return 0;
+        }
+
+        /// <summary>
+        /// 发送的字节信息
+        /// </summary>
+        public byte[] SendBytes { get; set; }
+    }
+}
---
+++ HslCommunication_Net45/Profinet/OpenProtocol/OpenProtocolNet.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HslCommunication.Core.Net;
+using HslCommunication.Core.IMessage;
+using HslCommunication.Core;
+using System.Net.Sockets;
+
+namespace HslCommunication.Profinet.OpenProtocol
+{
+    /// <summary>
+    /// 开放以太网协议
+    /// </summary>
+    public class OpenProtocolNet : NetworkDoubleBase<OpenProtocolMessage, RegularByteTransform>
+    {
+        #region Constructor
+
+        /// <summary>
+        /// 实例化一个默认的对象
+        /// </summary>
+        public OpenProtocolNet( )
+        {
+
+        }
+
+        /// <summary>
+        /// 使用指定的IP地址来初始化对象
+        /// </summary>
+        /// <param name="ipAddress">Ip地址</param>
+        /// <param name="port">端口号</param>
+        public OpenProtocolNet( string ipAddress, int port )
+        {
+
+        }
+
+        #endregion
+
+        #region Override NetworkDoubleBase
+
+        /// <summary>
+        /// 连接上服务器后需要进行的初始化操作
+        /// </summary>
+        /// <param name="socket">网络套接字</param>
+        /// <returns>是否初始化成功，依据具体的协议进行重写</returns>
+        protected override OperateResult InitializationOnConnect( Socket socket )
+        {
+            // 此处的 revison 应该等于多少？
+            OperateResult<string> open = ReadCustomer( 1, 0, 0, 0, null );
+            if (!open.IsSuccess) return open;
+
+            if (open.Content.Substring( 4, 4 ) == "0002")
+                return OperateResult.CreateSuccessResult( );
+            else
+                return new OperateResult( "Failed:" + open.Content.Substring( 4, 4 ) );
+        }
+
+        #endregion
+
+        /// <summary>
+        /// 自定义的命令读取
+        /// </summary>
+        /// <param name="mid"></param>
+        /// <param name="revison"></param>
+        /// <param name="stationId"></param>
+        /// <param name="spindleId"></param>
+        /// <param name="parameters"></param>
+        /// <returns></returns>
+        public OperateResult<string> ReadCustomer( int mid, int revison, int stationId, int spindleId, List<string> parameters )
+        {
+            if (parameters != null) parameters = new List<string>( );
+            OperateResult<byte[]> command = BuildReadCommand( mid, revison, stationId, spindleId, parameters );
+            if (!command.IsSuccess) return OperateResult.CreateFailedResult<string>( command );
+
+            OperateResult<byte[]> read = ReadFromCoreServer( command.Content );
+            if(!read.IsSuccess) return OperateResult.CreateFailedResult<string>( read );
+
+            return OperateResult.CreateSuccessResult( Encoding.ASCII.GetString( read.Content ) );
+        }
+
+
+        /// <summary>
+        /// 构建一个读取的初始报文
+        /// </summary>
+        /// <param name="mid"></param>
+        /// <param name="revison"></param>
+        /// <param name="stationId"></param>
+        /// <param name="spindleId"></param>
+        /// <param name="parameters"></param>
+        /// <returns></returns>
+        public static OperateResult<byte[]> BuildReadCommand( int mid, int revison, int stationId, int spindleId, List<string> parameters )
+        {
+            if (mid < 0 || mid > 9999) return new OperateResult<byte[]>( "Mid must be between 0 - 9999" );
+            if (revison < 0 || revison > 999) return new OperateResult<byte[]>( "revison must be between 0 - 999" );
+            if (stationId < 0 || stationId > 9) return new OperateResult<byte[]>( "stationId must be between 0 - 9" );
+            if (spindleId < 0 || spindleId > 99) return new OperateResult<byte[]>( "spindleId must be between 0 - 99" );
+
+            int count = 0;
+            if (parameters != null)
+                parameters.ForEach( m => count += m.Length );
+
+            StringBuilder sb = new StringBuilder( );
+            sb.Append( (20 + count).ToString( "D4" ) );
+            sb.Append( mid.ToString( "D4" ) );
+            sb.Append( revison.ToString( "D3" ) );
+            sb.Append( '\0' );
+            sb.Append( stationId.ToString( "D1" ) );
+            sb.Append( spindleId.ToString( "D2" ) );
+            sb.Append( '\0' );
+            sb.Append( '\0' );
+            sb.Append( '\0' );
+            sb.Append( '\0' );
+            sb.Append( '\0' );
+
+            if (parameters != null)
+                for (int i = 0; i < parameters.Count; i++)
+                {
+                    sb.Append( parameters[i] );
+                }
+            sb.Append( '\0' );
+            return OperateResult.CreateSuccessResult( Encoding.ASCII.GetBytes( sb.ToString( ) ) );
+        }
+
+    }
+}
