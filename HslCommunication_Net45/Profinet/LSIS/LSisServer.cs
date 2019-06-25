fae6c4331649cf1fd7d25a33f1eca1126789ee82
User <abdalamask30@gmail.com>
2019-06-25 12:28:41 +0200

    FIX LSisServer AND XGBFastEnet

--- HslCommunication_Net45/Profinet/LSIS/LSisServer.cs
+++ HslCommunication_Net45/Profinet/LSIS/LSisServer.cs
@@ -51,16 +51,49 @@ namespace HslCommunication.Profinet.LSIS
         /// <returns>byte数组值</returns>
         public override OperateResult<byte[]> Read(string address, ushort length)
         {
+            int startIndex = 0;
+            string[] fullAddressSplit = null;
+            string Dmain = string.Empty;
+            string AddressSplit = string.Empty;
             OperateResult<string> analysis = XGBFastEnet.AnalysisAddress(address, true);
             if (!analysis.IsSuccess) return OperateResult.CreateFailedResult<byte[]>(analysis);
+            if (address.IndexOf(".") > 0)//BitOnByte and BitOnWord
+            {
+                Dmain = address.Substring(0, 1);
+                AddressSplit = address.Remove(0, 2);
+                switch (Dmain)
+                {
+                    case "I":
+                    case "Q":
+                    case "U":
+                        {
+                            fullAddressSplit = AddressSplit.Split(new char[1] { '.' }, 4);
+                            startIndex = CheckAddress(fullAddressSplit[3]);
+                            break;
+                        }
+                    default:// MB---DB-----
+                        {
+                            fullAddressSplit = AddressSplit.Split(new char[1] { '.' }, 2);
+                            startIndex = CheckAddress(fullAddressSplit[0].Substring(2));
+                            break;
+                        }
+                }
 
-            int startIndex = CheckAddress(analysis.Content.Substring(3));
+            }
+            else
+            {
+                startIndex = CheckAddress(analysis.Content.Substring(3));
+            }
+
             switch (analysis.Content[1])
             {
-                case 'P': return OperateResult.CreateSuccessResult(pBuffer.GetBytes(startIndex, length));
-                case 'Q': return OperateResult.CreateSuccessResult(qBuffer.GetBytes(startIndex, length));
-                case 'M': return OperateResult.CreateSuccessResult(SoftBasic.BoolArrayToByte(mBuffer.GetBool(startIndex , length *= 8)));
-                case 'D': return OperateResult.CreateSuccessResult(dBuffer.GetBytes(startIndex==0 ? startIndex : startIndex*=2, length));
+                case 'I':
+                case 'Q':
+                case 'U':
+                case 'P':
+                    return OperateResult.CreateSuccessResult(SoftBasic.BoolArrayToByte(pBuffer.GetBool(startIndex, length *= 8)));
+                case 'M': return OperateResult.CreateSuccessResult(SoftBasic.BoolArrayToByte(mBuffer.GetBool(startIndex, length *= 8)));
+                case 'D': return OperateResult.CreateSuccessResult(dBuffer.GetBytes(startIndex == 0 ? startIndex : startIndex *= 2, length));
                 default: return new OperateResult<byte[]>(StringResources.Language.NotSupportedDataType);
             }
         }
@@ -73,15 +106,50 @@ namespace HslCommunication.Profinet.LSIS
         /// <returns>是否写入成功的结果对象</returns>
         public override OperateResult Write(string address, byte[] value)
         {
+            int startIndex = 0;
+            string[] fullAddressSplit = null;
+            string Dmain = string.Empty;
+            string AddressSplit = string.Empty;
             OperateResult<string> analysis = XGBFastEnet.AnalysisAddress(address, false);
             if (!analysis.IsSuccess) return OperateResult.CreateFailedResult<byte[]>(analysis);
+            if (address.IndexOf(".") > 0)//BitOnByte and BitOnWord
+            {
+                Dmain = address.Substring(0, 1);
+                AddressSplit = address.Remove(0, 2);
+                switch (Dmain)
+                {
+                    case "I":
+                    case "Q":
+                    case "U":
+                        {
+                            fullAddressSplit = AddressSplit.Split(new char[1] { '.' }, 4);
+                            startIndex = CheckAddress(fullAddressSplit[3]);
+                            break;
+                        }
+                    default:// MB---DB-----
+                        {
+                            fullAddressSplit = AddressSplit.Split(new char[1] { '.' }, 2);
+                            startIndex = CheckAddress(fullAddressSplit[0]);
+                            break;
+                        }
+                }
 
-            int startIndex = CheckAddress(analysis.Content.Substring(3));
+            }
+            else
+            {
+                startIndex = CheckAddress(analysis.Content.Substring(3));
+            }
+
+
             switch (analysis.Content[1])
             {
-                case 'P': pBuffer.SetBytes(value, startIndex); return OperateResult.CreateSuccessResult();
-                case 'Q': qBuffer.SetBytes(value, startIndex); return OperateResult.CreateSuccessResult();
-                case 'M': mBuffer.SetBool(value[0]==1 ? true:false, startIndex); return OperateResult.CreateSuccessResult();
+                case 'I':
+                case 'Q':
+                case 'U':
+                case 'P':
+                    pBuffer.SetBytes(value, startIndex); return OperateResult.CreateSuccessResult();
+              
+                case 'M': mBuffer.SetBool(value[0] == 1 ? true : false, startIndex); return OperateResult.CreateSuccessResult();
                 case 'D': dBuffer.SetBytes(value, startIndex == 0 ? startIndex : startIndex *= 2); return OperateResult.CreateSuccessResult();
                 default: return new OperateResult<byte[]>(StringResources.Language.NotSupportedDataType);
             }
@@ -213,14 +281,14 @@ namespace HslCommunication.Profinet.LSIS
                     if (receive[20] == 0x54)
                     {
                         // 读数据
-                         SendData = ReadByMessage(receive);
+                        SendData = ReadByMessage(receive);
                         RaiseDataReceived(SendData);
                         session.WorkSocket.Send(SendData);
-                       
+
                     }
                     else if (receive[20] == 0x58)
                     {
-                         SendData = WriteByMessage(receive);
+                        SendData = WriteByMessage(receive);
                         RaiseDataReceived(SendData);
                         session.WorkSocket.Send(SendData);
                     }
@@ -433,7 +501,7 @@ namespace HslCommunication.Profinet.LSIS
                 serialPort.Close();
             }
         }
-       
+
         /// <summary>
         /// 接收到串口数据的时候触发
         /// </summary>
@@ -441,8 +509,8 @@ namespace HslCommunication.Profinet.LSIS
         /// <param name="e">消息</param>
         private void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
         {
-           
-              var sp = (SerialPort)sender;
+
+            var sp = (SerialPort)sender;
 
             int rCount = 0;
             byte[] buffer = new byte[1024];
@@ -450,7 +518,7 @@ namespace HslCommunication.Profinet.LSIS
 
             while (true)
             {
-                System.Threading.Thread.Sleep(20);           
+                System.Threading.Thread.Sleep(20);
                 int count = sp.Read(buffer, rCount, sp.BytesToRead);
                 rCount += count;
                 if (count == 0) break;
@@ -476,15 +544,15 @@ namespace HslCommunication.Profinet.LSIS
                 SendData = WriteSerialByMessage(modbusCore);
                 RaiseDataReceived(SendData);
                 serialPort.Write(SendData, 0, SendData.Length);
-                
+
             }
             else
             {
                 serialPort.Close();
             }
-           
+
             if (IsStarted) RaiseDataSend(receive);
-            
+
         }
         public byte[] HexToBytes(string hex)
         {
@@ -512,13 +580,13 @@ namespace HslCommunication.Profinet.LSIS
             result.Add(0x53);    // command type: SB
             result.Add(0x42);
             result.AddRange(Encoding.ASCII.GetBytes("01"));
-           
 
-            int NameLength =int.Parse(Encoding.ASCII.GetString(command, 6, 2));
-            int RequestCount = Convert.ToInt32(Encoding.ASCII.GetString(command, 8 + NameLength, 2),16);
+
+            int NameLength = int.Parse(Encoding.ASCII.GetString(command, 6, 2));
+            int RequestCount = Convert.ToInt32(Encoding.ASCII.GetString(command, 8 + NameLength, 2), 16);
 
             string DeviceAddress = Encoding.ASCII.GetString(command, 9, NameLength - 1);
-            byte[] data = Read(DeviceAddress,(ushort) RequestCount).Content;
+            byte[] data = Read(DeviceAddress, (ushort)RequestCount).Content;
 
             result.AddRange(SoftBasic.BuildAsciiBytesFrom((byte)data.Length));
             result.AddRange(SoftBasic.BytesToAsciiBytes(data));
@@ -555,17 +623,17 @@ namespace HslCommunication.Profinet.LSIS
             else
             {
                 NameLength = Encoding.ASCII.GetString(packCommand, 6, 2);
-                DeviceAddress = Encoding.ASCII.GetString(packCommand,9, int.Parse(NameLength)-1);
-                
-                int RequestCount = int.Parse( Encoding.ASCII.GetString(packCommand, 8 + int.Parse(NameLength), 2));
-                string Value = Encoding.ASCII.GetString(packCommand, 8 + int.Parse(NameLength) +  RequestCount, RequestCount * 2);
+                DeviceAddress = Encoding.ASCII.GetString(packCommand, 9, int.Parse(NameLength) - 1);
+
+                int RequestCount = int.Parse(Encoding.ASCII.GetString(packCommand, 8 + int.Parse(NameLength), 2));
+                string Value = Encoding.ASCII.GetString(packCommand, 8 + int.Parse(NameLength) + RequestCount, RequestCount * 2);
                 var wdArys = HexToBytes(Value);
                 Write(DeviceAddress, wdArys);
-             }
+            }
 
 
-          
-            
+
+
             return result.ToArray();
         }
 
--- TestProject/HslCommunicationDemo/PLC/FormLsisFEnet.Designer.cs
+++ TestProject/HslCommunicationDemo/PLC/FormLsisFEnet.Designer.cs
@@ -91,6 +91,7 @@
             this.label6 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
             this.userControlHead1 = new HslCommunicationDemo.DemoControl.UserControlHead();
+            this.label2 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             this.groupBox4.SuspendLayout();
@@ -139,16 +140,17 @@
             // 
             // label22
             // 
-            this.label22.Location = new System.Drawing.Point(776, 3);
+            this.label22.Location = new System.Drawing.Point(712, 0);
             this.label22.Name = "label22";
-            this.label22.Size = new System.Drawing.Size(197, 45);
+            this.label22.Size = new System.Drawing.Size(261, 45);
             this.label22.TabIndex = 7;
-            this.label22.Text = "M100  D100 P100 T100 C100\r\nTo Read\r\n";
+            this.label22.Text = "XGB: M100  D100 P100 T100 C100 To Read\r\nXGK: MB0.0  DW0.0 IB0.0 QB0.0  To Read\r\n\r" +
+    "\n";
             // 
             // label21
             // 
             this.label21.AutoSize = true;
-            this.label21.Location = new System.Drawing.Point(702, 3);
+            this.label21.Location = new System.Drawing.Point(660, 3);
             this.label21.Name = "label21";
             this.label21.Size = new System.Drawing.Size(68, 17);
             this.label21.TabIndex = 6;
@@ -157,7 +159,7 @@
             // button2
             // 
             this.button2.Enabled = false;
-            this.button2.Location = new System.Drawing.Point(584, 7);
+            this.button2.Location = new System.Drawing.Point(563, 7);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(91, 28);
             this.button2.TabIndex = 5;
@@ -167,7 +169,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(477, 7);
+            this.button1.Location = new System.Drawing.Point(465, 7);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(91, 28);
             this.button1.TabIndex = 4;
@@ -373,6 +375,7 @@
             // 
             // groupBox2
             // 
+            this.groupBox2.Controls.Add(this.label2);
             this.groupBox2.Controls.Add(this.label17);
             this.groupBox2.Controls.Add(this.button24);
             this.groupBox2.Controls.Add(this.label19);
@@ -400,11 +403,11 @@
             // label17
             // 
             this.label17.AutoSize = true;
-            this.label17.Location = new System.Drawing.Point(26, 130);
+            this.label17.Location = new System.Drawing.Point(33, 123);
             this.label17.Name = "label17";
-            this.label17.Size = new System.Drawing.Size(162, 34);
+            this.label17.Size = new System.Drawing.Size(194, 34);
             this.label17.TabIndex = 19;
-            this.label17.Text = "Address: MX100  DW100  \r\nTW100  CW100  To Write";
+            this.label17.Text = "XGB: Address: MX100  DW100  \r\nTW100  CW100  To Write";
             // 
             // button24
             // 
@@ -741,7 +744,7 @@
             // 
             this.userControlHead1.BackColor = System.Drawing.Color.MediumPurple;
             this.userControlHead1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.userControlHead1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.userControlHead1.Font = new System.Drawing.Font("Microsoft YaHei", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.userControlHead1.Location = new System.Drawing.Point(0, 0);
             this.userControlHead1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.userControlHead1.MinimumSize = new System.Drawing.Size(800, 32);
@@ -749,6 +752,15 @@
             this.userControlHead1.ProtocolInfo = "Fast Enet";
             this.userControlHead1.Size = new System.Drawing.Size(1004, 32);
             this.userControlHead1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 169);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(186, 34);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "XGK: Address: MX0.1  DW0.1  \r\nTW0.0  CW0.0  To Write";
             // 
             // FormLsisFEnet
             // 
@@ -758,7 +770,7 @@
             this.Controls.Add(this.userControlHead1);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
-            this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Font = new System.Drawing.Font("Microsoft YaHei", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "FormLsisFEnet";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
@@ -845,5 +857,6 @@
         private System.Windows.Forms.Button button24;
         private System.Windows.Forms.Label label17;
         private DemoControl.UserControlHead userControlHead1;
+        private System.Windows.Forms.Label label2;
     }
 }
