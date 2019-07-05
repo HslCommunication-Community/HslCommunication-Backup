b6155e58a3db48907b6c33f8325377a247557378
dathlin <hsl200909@163.com>
2019-07-05 11:00:21 +0800

    fix(HslReflectionHelper): pass test, but run very slow

--- HslCommunication_Net45.Test/Core/Reflection/HslReflectionHelperTest.cs
+++ HslCommunication_Net45.Test/Core/Reflection/HslReflectionHelperTest.cs
@@ -7,6 +7,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using HslCommunication;
 using System.Reflection;
 using HslCommunication.Core;
+using System.Diagnostics;
 
 namespace HslCommunication_Net45.Test.Core.Reflection
 {
@@ -28,8 +29,24 @@ namespace HslCommunication_Net45.Test.Core.Reflection
             HslReflectionHelper.SetPropertyExp( IntProperty, myclass, 1234 );
             Assert.IsTrue( myclass.IntValue == 1234 );
 
-            HslReflectionHelper.SetPropertyExp( FloatProperty, myclass, 123.4 );
-            Assert.IsTrue( myclass.FloatValue == 123.4 );
+            HslReflectionHelper.SetPropertyExp( FloatProperty, myclass, 123.4f );
+            Assert.IsTrue( myclass.FloatValue == 123.4f );
+
+            //var sw = new Stopwatch( );
+            //sw.Start( );
+            //for (int i = 0; i < 100000; i++)
+            //{
+            //    IntProperty.SetValue( myclass, 123, null );
+            //}
+            //sw.Stop( );
+            //Console.WriteLine( "正常的情况：" + sw.ElapsedMilliseconds );
+            //sw.Restart( );
+            //for (int i = 0; i < 100000; i++)
+            //{
+            //    HslReflectionHelper.SetPropertyExp( IntProperty, myclass, 1234 );
+            //}
+            //sw.Stop( );
+            //Console.WriteLine( "表达式树的情况：" + sw.ElapsedMilliseconds );
         }
 
     }
