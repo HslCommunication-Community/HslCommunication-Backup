af6b1b41a3f60b79202d92432ad494ff5d6c668c
User <abdalamask30@gmail.com>
2019-08-10 21:06:45 +0300

    fix 2

--- TestProject/HslCommunicationDemo/PLC/FormLsisFEnet.cs
+++ TestProject/HslCommunicationDemo/PLC/FormLsisFEnet.cs
@@ -24,17 +24,7 @@ namespace HslCommunicationDemo
 
         private XGBFastEnet fastEnet = null;
 
-        private void linkLabel1_LinkClicked( object sender, LinkLabelLinkClickedEventArgs e )
-        {
-            try
-            {
-                System.Diagnostics.Process.Start( linkLabel1.Text );
-            }
-            catch (Exception ex) 
-            {
-                MessageBox.Show( ex.Message );
-            }
-        }
+        
 
         private void FormSiemens_Load( object sender, EventArgs e )
         {
@@ -42,12 +32,7 @@ namespace HslCommunicationDemo
 
             Language( Program.Language );
 
-            if (!Program.ShowAuthorInfomation)
-            {
-                label2.Visible = false;
-                linkLabel1.Visible = false;
-                label20.Visible = false;
-            }
+           
         }
 
 
@@ -56,10 +41,7 @@ namespace HslCommunicationDemo
             if (language == 2)
             {
                 Text = "Lsis Read PLC Demo";
-                label2.Text = "Blogs:";
-                label4.Text = "Protocols:";
-                label20.Text = "Author:Richard Hu";
-                label5.Text = "Fast Enet";
+               
 
                 label1.Text = "Ip:";
                 label3.Text = "Port:";
